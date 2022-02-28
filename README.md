333f0d04c07abfc1754c58942943e523f9af7993
Ellen <xiaoqin573@163.com>
2022-02-28 22:10:32 +0800

    基于wokerman + thinkphp的简单客服demo 需要安装redis 使用 6379无密码

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # wokerman-chat
-基于wokerman简单的客服聊天demo
+基于wokerman+thinkphp简单的客服聊天demo
+
+## 安装略 （需要 redis、thinkphp、wokerman）
+    请自行查阅资料
+## 启动wokerman
+![image-00](./chat-0.jpg)
+
+## 示例
+![image-01](./chat-1.jpg)
+
+
+![image-01](./chat-2.jpg)
---
+++ application/index/controller/Index.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\index\controller;
+
+use think\Controller;
+
+class Index extends Controller
+{
+    /**
+     * 聊天页面
+     * from-id 发送方用户id
+     * to-id 接收方用户id
+     * @return [type] [description]
+     */
+    public function index()
+    {
+        $fromId = input("from-id");
+        $toId = input("to-id");
+
+        $this->assign('fromId', $fromId);
+        $this->assign('toId', $toId);
+        
+        return $this->fetch();
+    }
+
+    /**
+     * 保存聊天数据 返回消息id
+     * @return [type] [description]
+     */
+    public function saveMsg()
+    {
+        // 这里用redis模拟的保存消息
+        $redis = new \Redis();
+        $redis->pconnect('127.0.0.1', 6379);
+
+        $cacheId = 'msg:incr:id:for:test';
+
+        $msgId = $redis->incr($cacheId);
+
+        $jsonArray = [
+            'code' => 200,
+            'msg' => '亲，消息发送成功',
+            'data' => [
+                'id' => $msgId
+            ]
+        ];
+        return json($jsonArray);
+    }
+}
